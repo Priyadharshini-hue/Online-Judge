4480dd441f708748a8eef83203848a97a4ca5d1b
Priyadharshini <priyadharshini.tnj98@gmail.com>
2023-09-09 19:03:03 +0530

    add Modal in AddProblem component

--- client/src/components/problems/AddProblem.jsx
+++ client/src/components/problems/AddProblem.jsx
@@ -1,51 +1,56 @@
 import React, { useState } from 'react';
-import { Card, Form, Button, InputGroup } from 'react-bootstrap';
+import { Card, Form, Button, InputGroup, Modal } from 'react-bootstrap';
 import axios from 'axios';
+import { BACK_SERVER_URL } from '../../config/config';
 
 const AddProblem = () => {
 
-    const [problemTitle, setProblemTitle] = useState('');
-    const [problemStatement, setProblemStatement] = useState('');
-    const [problemDifficulty, setProblemDifficulty] = useState('');
+    const [title, setTitle] = useState('');
+    const [statement, setStatement] = useState('');
+    const [difficulty, setDifficulty] = useState('');
     const [sampleInput, setSampleInput] = useState('');
     const [sampleOutput, setSampleOutput] = useState('');
     const [errors, setErrors] = useState({});
+    const [message, setMessage] = useState('');
+    const [show, setShow] = useState(false);
 
-    const handleProblemTitle = (e) => {
-        const newProblemTitle = e.target.value;
-        setProblemTitle(newProblemTitle);
-        validateProblemTitle(newProblemTitle);
+    const handleClose = () => setShow(false);
+
+    const handleTitle = (e) => {
+        const newTitle = e.target.value;
+        setTitle(newTitle);
+        validateTitle(newTitle);
     }
 
-    const validateProblemTitle = (value) => {
+    const validateTitle = (value) => {
         const errorsCopy = { ...errors };
         if (value.length > 120) {
-            errorsCopy.problemTitle = "You have reached your maximum limit of characters allowed";
+            errorsCopy.title = "You have reached your maximum limit of characters allowed";
         } else {
-            delete errorsCopy.problemTitle;
+            delete errorsCopy.title;
         }
         setErrors(errorsCopy);
     }
 
-    const handleProblemStatement = (e) => {
-        const newProblemStatement = e.target.value;
-        setProblemStatement(newProblemStatement);
-        validateProblemStatement(newProblemStatement);
+    const handleStatement = (e) => {
+        const newStatement = e.target.value;
+        setStatement(newStatement);
+        validateStatement(newStatement);
     }
 
-    const validateProblemStatement = (value) => {
+    const validateStatement = (value) => {
         const errorsCopy = { ...errors };
         if (value.length > 1000) {
-            errorsCopy.problemStatement = "You have reached your maximum limit of characters allowed";
+            errorsCopy.statement = "You have reached your maximum limit of characters allowed";
         } else {
-            delete errorsCopy.problemStatement;
+            delete errorsCopy.statement;
         }
         setErrors(errorsCopy);
     }
 
-    const handleProblemDifficulty = (e) => {
-        const newProblemDifficulty = e.target.value;
-        setProblemDifficulty(newProblemDifficulty);
+    const handleDifficulty = (e) => {
+        const newDifficulty = e.target.value;
+        setDifficulty(newDifficulty);
     }
 
     const handleSampleInput = (e) => {
@@ -86,11 +91,20 @@ const AddProblem = () => {
 
         if (Object.keys(errors).length === 0) {
             try {
-                const result = await axios.post('/addProblem', {
-                    problemTitle, problemStatement, problemDifficulty, sampleInput, sampleOutput
+                const result = await axios.post(`${BACK_SERVER_URL}/addProblem`, {
+                    title, statement, difficulty, sampleInput, sampleOutput
                 })
-                console.log(result.data);
-
+                if (result.status === 201) {
+                    setMessage("Problem exsists already!!");
+                } else {
+                    setMessage("Problem created successfully");
+                }
+                setShow(true);
+                setTitle('');
+                setStatement('');
+                setDifficulty('');
+                setSampleInput('');
+                setSampleOutput('');
             } catch (error) {
                 console.log(error);
             }
@@ -103,29 +117,29 @@ const AddProblem = () => {
                 <Form onSubmit={handleSubmit} >
                     <InputGroup className="mb-3" >
                         <InputGroup.Text>Problem Title</InputGroup.Text>
-                        <Form.Control type="text" required value={problemTitle} isInvalid={!!errors.problemTitle}
-                            onChange={handleProblemTitle} />
-                        {errors.problemTitle &&
+                        <Form.Control type="text" required value={title} isInvalid={!!errors.title}
+                            onChange={handleTitle} />
+                        {errors.title &&
                             <Form.Control.Feedback type="invalid">
-                                {errors.problemTitle}
+                                {errors.title}
                             </Form.Control.Feedback>
                         }
                     </InputGroup>
                     <InputGroup className="mb-3">
                         <InputGroup.Text>Problem Statement</InputGroup.Text>
                         <Form.Control as="textarea" aria-label="With textarea" rows={4} required
-                            value={problemStatement} isInvalid={!!errors.problemStatement}
-                            onChange={handleProblemStatement}
+                            value={statement} isInvalid={!!errors.statement}
+                            onChange={handleStatement}
                         />
-                        {errors.problemStatement &&
+                        {errors.statement &&
                             <Form.Control.Feedback type='invalid'>
-                                {errors.problemStatement}
+                                {errors.statement}
                             </Form.Control.Feedback>
                         }
                     </InputGroup >
                     <InputGroup className="mb-3" >
                         <InputGroup.Text>Problem Difficulty</InputGroup.Text>
-                        <Form.Select required value={problemDifficulty} onChange={handleProblemDifficulty}>
+                        <Form.Select required value={difficulty} onChange={handleDifficulty}>
                             <option> </option>
                             <option value="easy">easy</option>
                             <option value="medium">medium</option>
@@ -146,6 +160,12 @@ const AddProblem = () => {
                     </InputGroup>
                     <Button className="w-100" variant="outline-dark" type='submit'>Submit</Button>
                 </Form>
+                <Modal show={show} onHide={handleClose} centered>
+                    <Modal.Header closeButton />
+                    <Modal.Body className='d-flex '>
+                        <p>{message}</p>
+                    </Modal.Body>
+                </Modal>
             </Card>
         </div >
     )
