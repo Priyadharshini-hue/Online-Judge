38de4a84db0cd71fa6133557f65b0a4aae2c7f74
Priyadharshini <priyadharshini.tnj98@gmail.com>
2023-09-10 00:00:02 +0530

    add button to navigate from AddProblem page to ProblemList page

--- client/src/components/NavigationBar.jsx
+++ client/src/components/NavigationBar.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar'; 
+import { Container, Button } from 'react-bootstrap';
+import Navbar from 'react-bootstrap/Navbar';
 
-const NavigationBar = () => { 
+import { useLocation } from 'react-router-dom';
+const NavigationBar = () => {
+
+    const location = useLocation();
+    const routesWithButtons = ['/addProblem'];
+    const shouldRenderButtons = routesWithButtons.includes(location.pathname);
+
     return (
-        <Navbar className="bg-body-tertiary">
+        <Navbar className="bg-body-tertiary justify-content-between">
             <Container>
                 <Navbar.Brand>Online Judge</Navbar.Brand>
-            </Container>
+                <Navbar.Collapse className="justify-content-end">
+                    {shouldRenderButtons && (<Button variant="outline-dark" href="/getProblems">
+                        Go Back to Problems Page
+                    </Button>)}
+                    <div className="mx-2"></div>
+                    <Button variant="outline-dark" >Log out</Button>
+                </Navbar.Collapse>   </Container>
         </Navbar>
     );
 };
