0f780286ff6b33d4532a15041e00e0e8bcedc516
Priyadharshini <priyadharshini.tnj98@gmail.com>
2023-09-13 23:21:27 +0530

    add authentication middleware to problem routes

--- server/routes/problems.js
+++ server/routes/problems.js
@@ -1,10 +1,11 @@
 const express = require("express");
 
 const { addProblem, getProblems } = require("../controller/problemController");
+const auth = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
-router.post("/problems/add", addProblem);
-router.get("/problems/list", getProblems);
+router.post("/problems/add", auth, addProblem);
+router.get("/problems/list", auth, getProblems);
 
 module.exports = router;
