e0c732fa1e8e00aabe3d1007134b985453a42b36
Priyadharshini <priyadharshini.tnj98@gmail.com>
2023-09-14 23:49:08 +0530

    update NavigationBar component with authentication logic

--- client/src/components/NavigationBar.jsx
+++ client/src/components/NavigationBar.jsx
@@ -1,19 +1,26 @@
 import React from 'react';
 import { Container, Button } from 'react-bootstrap';
 import Navbar from 'react-bootstrap/Navbar';
+import { useAuth } from '../context/AuthContext';
 
 const NavigationBar = () => {
+    const token = sessionStorage.getItem("jwtToken");
+    const {logout} = useAuth();
+
     return (
         <Navbar className="bg-body-tertiary justify-content-between">
             <Container>
                 <Navbar.Brand>Online Judge</Navbar.Brand>
-                <Navbar.Collapse className="justify-content-end">
-                    <Button variant="outline-dark" href="/problems/list">
-                        Go Back to Problems Page
-                    </Button>
-                    <div className="mx-2"></div>
-                    <Button variant="outline-dark" >Log out</Button>
-                </Navbar.Collapse>   </Container>
+                {token ?
+                    <Navbar.Collapse className="justify-content-end">
+                        <Button variant="outline-dark" href="/problems/list">
+                            View Problems
+                        </Button>
+                        <div className="mx-2"></div>
+                        <Button variant="outline-dark" onClick={logout} >Log out</Button>
+                    </Navbar.Collapse>
+                    : null}
+            </Container>
         </Navbar>
     );
 };
