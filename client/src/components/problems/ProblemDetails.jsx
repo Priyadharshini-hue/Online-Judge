0724007beac9727bef7e758adcc68b8569c0d822
Priyadharshini <priyadharshini.tnj98@gmail.com>
2023-09-16 00:05:39 +0530

    add ProblemDetails component with the get request to fetch the problem info

---
+++ client/src/components/problems/ProblemDetails.jsx
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import { BACK_SERVER_URL } from "../../config/config"
+import { Badge, Card } from 'react-bootstrap';
+
+const ProblemDetails = () => {
+  const { problemId } = useParams();
+  const [problem, setProblem] = useState({});
+
+  useEffect(() => {
+    const fetchProblemDetails = async () => {
+      try {
+        const token = sessionStorage.getItem('jwtToken');
+        const headers = {
+          Authorization: `Bearer ${token}`,
+        };
+        console.log(problemId);
+        const response = await axios.get(`${BACK_SERVER_URL}/problems/${problemId}`, { headers });
+        setProblem(response.data);
+        console.log(response.data);
+      } catch (error) {
+        console.error("Error fetching problem details: ", error);
+      }
+    };
+
+    fetchProblemDetails();
+  }, [problemId]);
+
+  return (
+    <div className="d-flex flex-row ">
+      <div className="leftSide w-50 flex-grow-1 text-wrap">
+        <Card className=" ">
+          <Card.Body>
+            <Card.Title>{problem.title}</Card.Title>
+            <Card.Subtitle className="mb-2 text-muted"><Badge bg="dark" >{problem.difficulty}</Badge></Card.Subtitle>
+            <Card.Text  >{problem.statement}</Card.Text>
+            <Card.Text>Sample Input</Card.Text>
+            <Card.Text className="bg-dark text-white ">{problem.sampleInput}</Card.Text>
+            <Card.Text>Sample Output</Card.Text>
+            <Card.Text className="bg-dark text-white">{problem.sampleOutput}</Card.Text>
+          </Card.Body>
+        </Card>
+      </div>
+      <div className="rightSide flex-grow-1 w-50">compiler</div>
+    </div >
+  );
+};
+
+export default ProblemDetails;
