75db0de6c31f9c261ea40b7a09c4b41cc4adbc9f
Priyadharshini <priyadharshini.tnj98@gmail.com>
2023-09-15 23:59:58 +0530

    add delete route for deleting problem

--- server/routes/problems.js
+++ server/routes/problems.js
@@ -1,11 +1,16 @@
 const express = require("express");
 
-const { addProblem, getProblems } = require("../controller/problemController");
+const {
+  addProblem,
+  getProblems,
+  deleteProblem,
+} = require("../controller/problemController");
 const auth = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
 router.post("/problems/add", auth, addProblem);
 router.get("/problems/list", auth, getProblems);
+router.delete("/problems/:problemId", auth, deleteProblem);
 
 module.exports = router;
