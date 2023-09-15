77b7f30486ed95defef497d02cf771b9b801f089
Priyadharshini <priyadharshini.tnj98@gmail.com>
2023-09-15 23:52:34 +0530

    add createdBy and timestamps properties in problem schema

--- server/model/problemModel.js
+++ server/model/problemModel.js
@@ -1,27 +1,35 @@
 const mongoose = require("mongoose");
 
-const problemSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
+const problemSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    statement: {
+      type: String,
+      required: true,
+    },
+    difficulty: {
+      type: String,
+      required: true,
+    },
+    sampleInput: {
+      type: String,
+      required: true,
+    },
+    sampleOutput: {
+      type: String,
+      required: true,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
   },
-  statement: {
-    type: String,
-    required: true,
-  },
-  difficulty: {
-    type: String,
-    required: true,
-  },
-  sampleInput: {
-    type: String,
-    required: true,
-  },
-  sampleOutput: {
-    type: String,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 const Problem = mongoose.model("Problem", problemSchema);
 
