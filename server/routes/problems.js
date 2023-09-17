7073c1d7d77c06fa18cf6c2dc9653f5dd40356d4
Priyadharshini <priyadharshini.tnj98@gmail.com>
2023-09-17 23:49:41 +0530

    update problem route

--- server/routes/problems.js
+++ server/routes/problems.js
@@ -3,6 +3,8 @@ const express = require("express");
 const {
   addProblem,
   getProblems,
+  getProblem,
+  updateProblem,
   deleteProblem,
 } = require("../controller/problemController");
 const auth = require("../middleware/authMiddleware");
@@ -11,6 +13,9 @@ const router = express.Router();
 
 router.post("/problems/add", auth, addProblem);
 router.get("/problems/list", auth, getProblems);
+router.get("/problems/:problemId", auth, getProblem);
+router.get("/problems/edit/:problemId", auth, getProblem);
+router.put("/problems/edit/:problemId", auth, updateProblem);
 router.delete("/problems/:problemId", auth, deleteProblem);
 
 module.exports = router;
