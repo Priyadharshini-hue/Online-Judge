8454401e2839c7791b048c31377403b0921a4c44
Priyadharshini <priyadharshini.tnj98@gmail.com>
2023-09-24 00:12:11 +0530

    Replace axios with the getProblem function from the services/api module

--- client/src/components/problems/ProblemDetails.jsx
+++ client/src/components/problems/ProblemDetails.jsx
@@ -1,45 +1,47 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
 import { useParams } from "react-router-dom";
-import { BACK_SERVER_URL } from "../../config/config"
 import { Badge, Card } from 'react-bootstrap';
 import Compiler from "./Compiler";
+import { useAuth } from "../../context/AuthContext";
+import { getProblem } from "../../services/api";
 
 const ProblemDetails = () => {
   const { problemId } = useParams();
   const [problem, setProblem] = useState({});
+  const { token } = useAuth();
 
   useEffect(() => {
     const fetchProblemDetails = async () => {
       try {
-        const token = sessionStorage.getItem('jwtToken');
-        const headers = {
-          Authorization: `Bearer ${token}`,
-        };
-        console.log(problemId);
-        const response = await axios.get(`${BACK_SERVER_URL}/problems/${problemId}`, { headers });
-        setProblem(response.data);
-        console.log(response.data);
+        const problemData = await getProblem(problemId, token);
+        setProblem(problemData);
       } catch (error) {
         console.error("Error fetching problem details: ", error);
       }
     };
 
     fetchProblemDetails();
-  }, [problemId]);
+  }, [problemId, token]);
+
+  const testCases = problem.testCases || [];
 
   return (
     <div className="d-flex flex-row ">
-      <div className="leftSide w-50 flex-grow-1 text-wrap">
-        <Card className=" ">
-          <Card.Body>
+      <div className="overflow-auto leftSide h-100 w-50 flex-grow-1 text-wrap" style={{ maxHeight: "550px" }}>
+        <Card className="m-2">
+          <Card.Body >
             <Card.Title>{problem.title}</Card.Title>
             <Card.Subtitle className="mb-2 text-muted"><Badge bg="dark" >{problem.difficulty}</Badge></Card.Subtitle>
             <Card.Text  >{problem.statement}</Card.Text>
-            <Card.Text>Sample Input</Card.Text>
-            <Card.Text className="bg-dark text-white ">{problem.sampleInput}</Card.Text>
-            <Card.Text>Sample Output</Card.Text>
-            <Card.Text className="bg-dark text-white">{problem.sampleOutput}</Card.Text>
+            {testCases.slice(0, 2).map((testCase, index) => (
+              <div key={index}>
+                <Card.Text className="mt-3 mb-0">Test case {index + 1}</Card.Text>
+                <Card.Text className="m-0">Input</Card.Text>
+                <Card.Text className="m-0 bg-dark text-white">{testCase.input}</Card.Text>
+                <Card.Text className="m-0">Output</Card.Text>
+                <Card.Text className="bg-dark text-white">{testCase.output}</Card.Text>
+              </div>
+            ))}
           </Card.Body>
         </Card>
       </div>
--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,19 +1,36 @@
 import axios from "axios";
 import { BACK_SERVER_URL } from "../config/config";
 
-export async function createProblem(problemData, token) {
+export const createProblem = async (problemData, token) => {
   try {
     const headers = {
       Authorization: `Bearer ${token}`,
     };
-    
-    const result = await axios.post(
+
+    const response = await axios.post(
       `${BACK_SERVER_URL}/problems/add`,
       problemData,
       { headers }
     );
-    return result;
+    return response;
   } catch (error) {
-    console.log(error);
+    return error;
   }
-}
+};
+
+export const getProblem = async (problemId, token) => {
+  try {
+    const headers = {
+      Authorization: `Bearer ${token}`,
+    };
+
+    const response = await axios.get(
+      `${BACK_SERVER_URL}/problems/${problemId}`,
+      { headers }
+    );
+
+    return response.data;
+  } catch (error) {
+    return error;
+  }
+};
