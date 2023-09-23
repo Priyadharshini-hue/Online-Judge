6fa60f23add6a9969d7b96c052461864b82051f4
Priyadharshini <priyadharshini.tnj98@gmail.com>
2023-09-24 00:35:44 +0530

    split the component into ProblemForm, TestCaseList, AddTestCaseModal components and move state and related functions into useProblemState custom hook

--- client/src/components/problems/ProblemEdit.jsx
+++ client/src/components/problems/ProblemEdit.jsx
@@ -1,138 +1,79 @@
 import React, { useState, useEffect } from 'react';
-import { Card, Form, Button, InputGroup, Modal } from 'react-bootstrap';
-import axios from 'axios';
-import { BACK_SERVER_URL } from '../../config/config';
-import { useParams } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
+import { Card, Form, Button, Modal } from 'react-bootstrap';
+import { useParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../../context/AuthContext';
+import { useProblemState } from './ProblemState';
+import { useTestCaseState } from './TestCaseState';
+import ProblemForm from './ProblemForm';
+import TestCaseList from './TestCaseList';
+import AddTestCaseModal from './AddTestCaseModal';
+import { getProblemToEdit, updateProblem } from '../../services/api';
 
 const ProblemEdit = () => {
-    const { problemId } = useParams(); 
-    const [editedTitle, setEditedTitle] = useState("");
-    const [editedStatement, setEditedStatement] = useState("");
-    const [editedDifficulty, setEditedDifficulty] = useState("");
-    const [editedSampleInput, setEditedSampleInput] = useState("");
-    const [editedSampleOutput, setEditedSampleOutput] = useState("");
-    const [message, setMessage] = useState('');
-    const [show, setShow] = useState(false);
-
-    const handleClose = () => setShow(false);
-    const [errors, setErrors] = useState({});
+    const { problemId } = useParams();
+    const { problemState, setProblemState, handleInputChange, deleteTestCase } = useProblemState();
+    const { validateTestCase, setTestCaseModalState, testCaseModalState, handleShowTestCaseModal,
+        handleTestCaseInputChange, handleCloseTestCaseModal } = useTestCaseState();
+    const { token } = useAuth();
     const navigate = useNavigate();
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const [message, setMessage] = useState('');
 
     useEffect(() => {
         const fetchProblemDetails = async () => {
             try {
-                const token = sessionStorage.getItem('jwtToken');
-                const headers = {
-                    Authorization: `Bearer ${token}`,
-                };
-                console.log(problemId);
-                const response = await axios.get(`${BACK_SERVER_URL}/problems/edit/${problemId}`,
-                    { headers });
- 
-                setEditedTitle(response.data.title);
-                setEditedStatement(response.data.statement);
-                setEditedDifficulty(response.data.difficulty);
-                setEditedSampleInput(response.data.sampleInput);
-                setEditedSampleOutput(response.data.sampleOutput);
+                const problemData = await getProblemToEdit(problemId, token);
 
-                console.log(response.data);
+                setProblemState({
+                    ...problemState,
+                    title: problemData.title,
+                    statement: problemData.statement,
+                    difficulty: problemData.difficulty,
+                    testCases: problemData.testCases
+                });
+
+                console.log(problemData);
             } catch (error) {
-                console.error("Error fetching problem details: ", error);
+                console.error('Error fetching problem details: ', error);
             }
         };
 
         fetchProblemDetails();
-    }, [problemId]);
-
-    const handleTitleChange = (e) => {
-        setEditedTitle(e.target.value);
-        validateTitle(e.target.value);
-    };
-
-    const validateTitle = (value) => {
-        const errorsCopy = { ...errors };
-        if (value.length > 120) {
-            errorsCopy.title = "You have reached your maximum limit of characters allowed";
-        } else {
-            delete errorsCopy.title;
-        }
-        setErrors(errorsCopy);
-    }
-
-    const handleStatementChange = (e) => {
-        setEditedStatement(e.target.value);
-        validateStatement(e.target.value);
-    }
-
-    const validateStatement = (value) => {
-        const errorsCopy = { ...errors };
-        if (value.length > 1000) {
-            errorsCopy.statement = "You have reached your maximum limit of characters allowed";
-        } else {
-            delete errorsCopy.statement;
-        }
-        setErrors(errorsCopy);
-    }
-
-    const handleDifficultyChange = (e) => {
-        setEditedDifficulty(e.target.value);
-    }
-
-    const handleSampleInputChange = (e) => {
-        setEditedSampleInput(e.target.value);
-        validateSampleInput(e.target.value);
-    }
-
-    const validateSampleInput = (value) => {
-        const errorsCopy = { ...errors };
-        if (value.length > 1000) {
-            errorsCopy.sampleInput = "You have reached your maximum limit of characters allowed";
-        } else {
-            delete errorsCopy.sampleInput;
-        }
-        setErrors(errorsCopy);
-    }
-
-    const handleSampleOutputChange = (e) => {
-        setEditedSampleOutput(e.target.value);
-        validateSampleOutput(e.target.value);
-    }
-
-    const validateSampleOutput = (value) => {
-        const errorsCopy = { ...errors };
-        if (value.length > 1000) {
-            errorsCopy.sampleOutput = "You have reached your maximum limit of characters allowed";
-        } else {
-            delete errorsCopy.sampleOutput;
-        }
-        setErrors(errorsCopy);
-    }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [problemId, token]);
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        console.log(e);
+        console.log(problemState.testCases);
+        if (problemState.testCases.length === 0) {
+            setProblemState({
+                ...problemState,
+                errors: {
+                    ...problemState.errors,
+                    testCase: 'Please add at least one test case before submitting'
+                }
+            });
+            return;
+        }
 
-        if (Object.keys(errors).length === 0) {
+        if (Object.values(problemState.errors).every((error) => error === '')) {
             try {
-                const token = sessionStorage.getItem('jwtToken');
-                const headers = {
-                    Authorization: `Bearer ${token}`,
-                };
-                const result = await axios.put(`${BACK_SERVER_URL}/problems/edit/${problemId}`, {
-                    title: editedTitle,
-                    statement: editedStatement,
-                    difficulty: editedDifficulty,
-                    sampleInput: editedSampleInput,
-                    sampleOutput: editedSampleOutput
-                }, { headers });
-                if (result.status === 200) {
-                    console.log("Problem edited!!");
-                    setMessage("Problem edited successfully")
-                    setShow(true); 
 
+                const result = await updateProblem(token, problemId, {
+                    title: problemState.title,
+                    statement: problemState.statement,
+                    difficulty: problemState.difficulty,
+                    testCases: problemState.testCases
+                });
+
+                if (result.status === 200) {
+                    console.log('Problem edited!!');
+                    setMessage('Problem edited successfully')
+                    setShow(true);
                     setTimeout(() => {
-                        navigate("/problems/list");
+                        navigate('/problems/list');
                     }, 3000);
                 }
             } catch (error) {
@@ -140,61 +81,67 @@ const ProblemEdit = () => {
             }
         }
     }
+
+    const handleSubmitTestCaseModal = (e) => {
+        e.preventDefault();
+        const testCaseErrors = validateTestCase();
+
+        if (Object.keys(testCaseErrors).length === 0) {
+            if (problemState.errors.testCase) {
+                delete problemState.errors.testCase;
+            }
+            setProblemState({
+                ...problemState,
+                testCases: [...problemState.testCases, testCaseModalState.data]
+            });
+
+            console.log(problemState);
+            setTestCaseModalState({
+                show: false,
+                data: { input: '', output: '' },
+                errors: {},
+            });
+        } else {
+            setTestCaseModalState({
+                ...testCaseModalState,
+                errors: testCaseErrors,
+            });
+        }
+    };
+
     return (
         <div className='mt-2 d-flex flex-wrap justify-content-center '>
-            <Card className="p-3 w-100">
+            <Card className='p-3 w-100'>
                 <Form onSubmit={handleSubmit} >
-                    <InputGroup className="mb-3" >
-                        <InputGroup.Text>Problem Title</InputGroup.Text>
-                        <Form.Control type="text" required name="title" value={editedTitle}
-                            onChange={handleTitleChange} isInvalid={!!errors.title} />
-                        {errors.title &&
-                            <Form.Control.Feedback type="invalid">
-                                {errors.title}
-                            </Form.Control.Feedback>
-                        }
-                    </InputGroup>
-                    <InputGroup className="mb-3">
-                        <InputGroup.Text>Problem Statement</InputGroup.Text>
-                        <Form.Control as="textarea" aria-label="With textarea" rows={5} required
-                            value={editedStatement} isInvalid={!!errors.statement}
-                            onChange={handleStatementChange}
-                        />
-                        {errors.statement &&
-                            <Form.Control.Feedback type='invalid'>
-                                {errors.statement}
-                            </Form.Control.Feedback>
-                        }
-                    </InputGroup >
-                    <InputGroup className="mb-3" >
-                        <InputGroup.Text>Problem Difficulty</InputGroup.Text>
-                        <Form.Select required value={editedDifficulty} onChange={handleDifficultyChange}>
-                            <option> </option>
-                            <option value="easy">easy</option>
-                            <option value="medium">medium</option>
-                            <option value="hard">hard</option>
-                        </Form.Select>
-                    </InputGroup>
-                    <InputGroup className="mb-3">
-                        <InputGroup.Text>Sample Input</InputGroup.Text>
-                        <Form.Control as="textarea" aria-label="With textarea" rows={4} required
-                            value={editedSampleInput} onChange={handleSampleInputChange} isInvalid={!!errors.sampleInput} />
-                    </InputGroup>
-                    <InputGroup className="mb-3">
-                        <InputGroup.Text>Sample output</InputGroup.Text>
-                        <Form.Control as="textarea" aria-label="With textarea" rows={3} required
-                            value={editedSampleOutput} onChange={handleSampleOutputChange} isInvalid={!!errors.sampleOutput} />
-                    </InputGroup>
-                    <Button className="w-100" variant="outline-dark" type='submit'>Submit</Button>
+                    <ProblemForm problemState={problemState} handleInputChange={handleInputChange} />
+                    <Button className='mb-3' variant='outline-dark' onClick={handleShowTestCaseModal}>
+                        Add Test Case</Button>
+                    {problemState.errors && problemState.errors.testCase && (
+                        <div className="text-danger mb-3">
+                            Please add at least one test case before submitting.
+                        </div>
+                    )}
+                    <TestCaseList testCases={problemState.testCases}
+                        deleteTestCase={deleteTestCase} />
+                    <Button className='w-100' variant='outline-dark' type='submit'
+                        disabled={Object.values(problemState.errors).some((error) => error !== '')}>
+                        Submit</Button>
                 </Form>
-                <Modal backdrop="static"
-                    keyboard={false} show={show} onHide={handleClose} centered>
-                    <Modal.Header closeButton />
-                    <Modal.Body className='d-flex '>
-                        <p>{message}</p>
-                    </Modal.Body>
-                </Modal>
             </Card>
+            <AddTestCaseModal show={testCaseModalState.show}
+                handleClose={handleCloseTestCaseModal}
+                handleSubmit={handleSubmitTestCaseModal}
+                testCaseData={testCaseModalState.data}
+                handleInputChange={handleTestCaseInputChange}
+                testCaseError={testCaseModalState.errors}
+            />
+            <Modal backdrop='static'
+                keyboard={false} show={show} onHide={handleClose} centered>
+                <Modal.Header closeButton />
+                <Modal.Body className='d-flex '>
+                    <p>{message}</p>
+                </Modal.Body>
+            </Modal>
         </div >
     )
 }
