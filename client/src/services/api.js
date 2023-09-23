7c28f4c02c0fce49195ebbf46760c09ef5e7533c
Priyadharshini <priyadharshini.tnj98@gmail.com>
2023-09-24 01:11:45 +0530

    update api file

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -18,6 +18,23 @@ export const createProblem = async (problemData, token) => {
   }
 };
 
+export const getProblemToEdit = async (problemId, token) => {
+  try {
+    const headers = {
+      Authorization: `Bearer ${token}`,
+    };
+
+    const response = await axios.get(
+      `${BACK_SERVER_URL}/problems/edit/${problemId}`,
+      { headers }
+    );
+
+    return response.data;
+  } catch (error) {
+    return error;
+  }
+};
+
 export const getProblem = async (problemId, token) => {
   try {
     const headers = {
@@ -33,4 +50,52 @@ export const getProblem = async (problemId, token) => {
   } catch (error) {
     return error;
   }
+};
+
+export const updateProblem = async (token, problemId, problemData) => {
+  try {
+    const headers = {
+      Authorization: `Bearer ${token}`,
+    };
+
+    const response = await axios.put(
+      `${BACK_SERVER_URL}/problems/edit/${problemId}`,
+      problemData,
+      { headers }
+    );
+
+    return response;
+  } catch (error) {
+    return error;
+  }
+};
+
+export const fetchProblems = async (token) => {
+  try {
+    const headers = {
+      Authorization: `Bearer ${token}`,
+    };
+    const response = await axios.get(`${BACK_SERVER_URL}/problems/list`, {
+      headers,
+    });
+    return response.data.data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+export const deleteProblem = async (token, problemId) => {
+  try {
+    const headers = {
+      Authorization: `Bearer ${token}`,
+    };
+    const response = await axios.delete(
+      `${BACK_SERVER_URL}/problems/${problemId}`,
+      {
+        headers,
+      }
+    );
+    return response.status === 200;
+  } catch (error) {}
 };
