387d3601ef1e03ddf716c13de36b42dfb1057c1d
Priyadharshini <priyadharshini.tnj98@gmail.com>
2023-09-26 22:55:52 +0530

    update authState

--- client/src/components/auth/authState.js
+++ client/src/components/auth/authState.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-export function useForgotPasswordFormState() {
+export const useForgotPasswordFormState = () => {
   const [forgotPasswordFormState, SetForgotPasswordFormState] = useState({
     email: "",
     errorMessage: "",
@@ -32,9 +32,9 @@ export function useForgotPasswordFormState() {
     SetForgotPasswordFormState,
     handleInputChange,
   };
-}
+};
 
-export function useResetPasswordFormState() {
+export const useResetPasswordFormState = () => {
   const [resetPasswordFormState, setResetPasswordFormState] = useState({
     password: "",
     confirmPassword: "",
@@ -88,4 +88,60 @@ export function useResetPasswordFormState() {
     setResetPasswordFormState,
     handleInputChange,
   };
-}
+};
+
+export const useSignUpFormState = () => {
+  const [signUpFormState, setSignUpFormState] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    errors: {},
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setSignUpFormState((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+    validateField(name, value);
+  };
+
+  const validateField = (name, value) => {
+    const errorMessage = generateErrorMessage(name, value); 
+
+    setSignUpFormState((prevState) => ({
+      ...prevState,
+      errors: {
+        ...prevState.errors,
+        [name]: errorMessage,
+      },
+    }));
+  };
+
+  const generateErrorMessage = (name, value) => {
+    if (name === "name") {
+      if (value.length < 3) {
+        return "Name must be at least 3 characters";
+      } else if (value.length > 20) {
+        return "Name must be less than 20 characters";
+      }
+    } else if (name === "email") {
+      const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+      return emailPattern.test(value) ? "" : "Invalid email address";
+    } else if (name === "password") {
+      if (value.trim().length !== value.length) {
+        return "Password should not contain whitespace";
+      } else if (value.length < 8) {
+        return "Password is too short (min. 8 characters)";
+      }
+    } else if (name === "confirmPassword") {
+      if (value !== signUpFormState.password) {
+        return "Passwords do not match";
+      }
+    }
+    return "";
+  };
+  return { signUpFormState, setSignUpFormState, handleInputChange };
+};
