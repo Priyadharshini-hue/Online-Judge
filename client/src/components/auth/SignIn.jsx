7ebd74d0ef2878502ba177089b2685f3008fc830
Priyadharshini <priyadharshini.tnj98@gmail.com>
2023-09-26 22:53:52 +0530

    move api call to  from the services/api module

--- client/src/components/auth/SignIn.jsx
+++ client/src/components/auth/SignIn.jsx
@@ -1,10 +1,9 @@
 import React, { useState } from 'react';
 import { Card, Form, Button, Image, Alert } from 'react-bootstrap';
-import { useNavigate } from "react-router-dom";
-import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 import image from '../../images/signIn.svg';
-import { BACK_SERVER_URL } from '../../config/config'
 import { useAuth } from '../../context/AuthContext';
+import { getUser } from '../../services/api';
 
 const SignIn = () => {
 
@@ -19,93 +18,84 @@ const SignIn = () => {
 
     const inputValidation = () => {
         const errors = {};
-
         const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-        if (!email.trim()) {
-            errors.email = "Email is required";
-        } else if (!emailPattern.test(email)) {
-            errors.email = "Invalid email address";
+        if (!emailPattern.test(email)) {
+            errors.email = 'Invalid email address';
         }
-
-        if (!password.trim()) {
-            errors.password = "Password is required";
-        } else if (password.trim().length !== password.length) {
-            errors.password = "Password Should not contain whitespace";
+        if (password.trim().length !== password.length) {
+            errors.password = 'Password Should not contain whitespace';
         }
-
         setErrors(errors);
-        // console.log(errors);
+        console.log(errors);
         return Object.keys(errors).length === 0;
     }
 
-    const handleSubmit = (event) => {
+    const handleSubmit = async (event) => {
 
         event.preventDefault();
-        // console.log(event);
 
         if (inputValidation()) {
-            axios.post(`${BACK_SERVER_URL}/user/signIn`, { email: email, password: password })
-                .then(result => {
-                    if (result.status === 201) {
-                        setMessage('Incorrect Password !!')
-                        setPassword("");
-                    } else if (result.status === 202) {
-                        setMessage("User not found !!")
-                    } else if (result.status === 200) {
-                        // console.log(result);
-                        login(result.data.token);
+            const result = await getUser({ email, password });
+            console.log(result);
+            if (result.status === 201) {
+                setMessage(result.data.message);
+            } else if (result.status === 202) {
+                setMessage(result.data.message);
+            } else if (result.status === 200) {
+                setMessage(result.data.message);
+                login(result.data.token, result.data.userId);
 
-                        setMessage('Signing in....');
-                        setPassword("");
-                        setEmail("");
-                        try {
-                            navigate("/problems/list");
-                        } catch (err) {
-                            console.log(err);
-                        }
-                    }
-                })
-                .catch(err => { console.log(err); })
+                setPassword('');
+                setEmail('');
+                try {
+                    navigate('/problems/list');
+                } catch (err) {
+                    console.log(err);
+                }
+            }
         }
     }
+
+    const handleInputFocus = () => setMessage('');
+
     return (
         <div className='mt-3 d-flex justify-content-center '>
-            <Card className="p-3 w-25">
-                <Image src={image} rounded style={{ display: 'block', margin: '0 auto', width: 80, height: 80, borderRadius: '50%', padding: "10px" }} />
+            <Card className='p-3 w-25'>
+                <Image src={image} rounded style={{ display: 'block', margin: '0 auto', width: 80, height: 80, borderRadius: '50%', padding: '10px' }} />
                 {message && (
-                    <Alert className='text-center' variant="danger">
+                    <Alert className='text-center' variant='danger'>
                         {message}
                     </Alert>
                 )}
-                <Form onSubmit={handleSubmit}>
-                    <Form.Group className="mb-3" controlId="formGroupEmail" placeholder="Email address">
+                <Form onSubmit={handleSubmit} onFocus={handleInputFocus}>
+                    <Form.Group className='mb-3' controlId='formGroupEmail' placeholder='Email address'>
                         <Form.Label>Email address</Form.Label>
-                        <Form.Control required type="email" placeholder="Enter email"
+                        <Form.Control required type='email' placeholder='Enter email'
                             value={email} onChange={(e) => setEmail(e.target.value)} isInvalid={!!errors.email} />
                         {errors.email &&
-                            <Form.Control.Feedback type="invalid">
+                            <Form.Control.Feedback type='invalid'>
                                 {errors.email}
                             </Form.Control.Feedback>
                         }
                     </Form.Group>
-                    <Form.Group className="mb-3" controlId="formGroupPassword">
+                    <Form.Group className='mb-3' controlId='formGroupPassword'>
                         <Form.Label>Password</Form.Label>
-                        <Form.Control required type="password" placeholder="Password"
+                        <Form.Control required type='password' placeholder='Password'
                             value={password} onChange={(e) => setPassword(e.target.value)} isInvalid={!!errors.password} />
                         {errors.password &&
-                            <Form.Control.Feedback type="invalid">
+                            <Form.Control.Feedback type='invalid'>
                                 {errors.password}
                             </Form.Control.Feedback>
                         }
                     </Form.Group>
-                    <Button className="w-100" variant="outline-dark" type='submit'>Submit</Button>
-                    <Form.Text className=" d-flex justify-content-center mt-2">
-                        <span className=""> Don't have an account?</span> &nbsp;
-                        <a className="fw-bold text-dark text-decoration-none" href="/user/signUp">Sign up</a>
+                    <Button className='w-100' variant='outline-dark' type='submit'>Submit</Button>
+                    <Form.Text className=' d-flex justify-content-center mt-2'>
+                        <span className=''> Don't have an account?</span> &nbsp;
+                        <a className='fw-bold text-dark text-decoration-none' href='/user/signUp'>Sign up</a>
                     </Form.Text>
-                    <Form.Text className=" d-flex justify-content-center mt-2">
-                        <span className="">Forgot Password? </span> &nbsp;
-                        <a className="fw-bold text-dark text-decoration-none" href="/resetPassword">Reset</a>
+                    <Form.Text className=' d-flex justify-content-center mt-2'>
+                        <span className=''>Forgot Password? </span> &nbsp;
+                        <a className='fw-bold text-dark text-decoration-none' href='/user/forgotPassword'>Reset</a>
                     </Form.Text>
                 </Form>
             </Card>
