f7b66f29865b1f08f574aaadf573a70412955269
Priyadharshini <priyadharshini.tnj98@gmail.com>
2023-09-26 23:00:23 +0530

    remove onFocus functionality

--- client/src/components/problems/ProblemForm.jsx
+++ client/src/components/problems/ProblemForm.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Form, InputGroup } from 'react-bootstrap';
 
-const ProblemForm = ({ problemState, handleInputChange, handleInputFocus }) => {
+const ProblemForm = ({ problemState, handleInputChange }) => {
     return (
         <div>
             <InputGroup className='mb-3' >
                 <InputGroup.Text>Problem Title</InputGroup.Text>
                 <Form.Control type='text' required name='title' value={problemState.title}
                     isInvalid={!!problemState.errors && !!problemState.errors.title}
-                    onChange={handleInputChange} onFocus={handleInputFocus} />
+                    onChange={handleInputChange} />
                 {problemState.errors && problemState.errors.title &&
                     <Form.Control.Feedback type='invalid'>
                         {problemState.errors.title}
