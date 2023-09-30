ccbce88c4215841c4769881065090adddabacdb5
Priyadharshini <priyadharshini.tnj98@gmail.com>
2023-09-30 19:51:58 +0530

    add dropdown in navigation bar

--- client/src/components/NavigationBar.jsx
+++ client/src/components/NavigationBar.jsx
@@ -1,25 +1,39 @@
 import React from 'react';
-import { Container, Button } from 'react-bootstrap';
+import { Container, Button, NavDropdown } from 'react-bootstrap';
 import Navbar from 'react-bootstrap/Navbar';
 import { useAuth } from '../context/AuthContext';
+import { Link } from 'react-router-dom';
 
 const NavigationBar = () => {
     const token = sessionStorage.getItem("jwtToken");
-    const {logout} = useAuth();
+    const { logout } = useAuth();
 
     return (
         <Navbar className="bg-body-tertiary justify-content-between">
             <Container>
                 <Navbar.Brand>Online Judge</Navbar.Brand>
-                {token ?
+                {token ? (
                     <Navbar.Collapse className="justify-content-end">
-                        <Button variant="outline-dark" href="/problems/list">
-                            View Problems
-                        </Button>
+                        <NavDropdown title="Explore" id="basic-nav-dropdown">
+                            <NavDropdown.Item as={Link} to="/problems/add">
+                                Add Problem
+                            </NavDropdown.Item>
+                            <NavDropdown.Item as={Link} to="/problems/list">
+                                View Problems
+                            </NavDropdown.Item>
+                            <NavDropdown.Item as={Link} to="/submissions/history">
+                                Submission History
+                            </NavDropdown.Item>
+                            <NavDropdown.Item as={Link} to="/submissions/leaderBoard">
+                                Leader board
+                            </NavDropdown.Item>
+                        </NavDropdown>
                         <div className="mx-2"></div>
-                        <Button variant="outline-dark" onClick={logout} >Log out</Button>
+                        <Button variant="outline-dark" onClick={logout}>
+                            Log out
+                        </Button>
                     </Navbar.Collapse>
-                    : null}
+                ) : null}
             </Container>
         </Navbar>
     );
