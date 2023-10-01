0f3b15083d4210fcf0fcd6f44b5fc3dca828c68b
Priyadharshini <priyadharshini.tnj98@gmail.com>
2023-10-01 23:39:37 +0530

    update route

--- server/routes/code.js
+++ server/routes/code.js
@@ -1,12 +1,10 @@
 const express = require("express");
-
-const { runProblem } = require("../controller/runController");
+ 
 const { submitProblem } = require("../controller/submitController");
 const auth = require("../middleware/authMiddleware");
 
 const router = express.Router();
-
-router.post("/problem/run", auth, runProblem);
+ 
 router.post("/problem/submit", auth, submitProblem);
 
 module.exports = router;
